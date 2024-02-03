3597a5ce49d74d0c969ed5bf388877d3b43bd64d
Laurah Chege <laurachege9@gmail.com>
2024-02-03 16:28:19 +0300

    script change

--- cliscript.sh
+++ cliscript.sh
@@ -1,31 +1,17 @@
 #!/bin/bash
 
-if [[ $# -eq 0 ]]; then
-    echo '[X] No arguments provided'
-    echo '[!] Usage: ./cliscript <container-name> <file-name1>'
-    exit 1
-elif [[ $# -eq 1 ]]; then
-    echo '[X] Too few arguments provided!'
-    echo '[!] Usage: ./cliscript <container-name> <file-name1>'
-    exit 1
-elif [[ $# -ge 3 ]]; then
-    echo '[X] Too many arguments provided!'
-    echo '[!] Usage: ./cliscript <container-name> <file-name1>'
-    exit 1
-elif [[ ! -f $2 ]]; then
-    echo '[X] File does not exist!'
-    exit 1
-else
-    does_exist=$(az storage blob exists --container-name $1 --name $2 --auth-mode login | grep exists)
-    if [[ $does_exist == *"true"* ]]; then
-        echo '[!] File with the same name already exists!'
-        exit 1
-    else
-        echo '[+] Uploading file to Azure Blob Storage...'
-        az storage blob upload --container-name $1 --file $2 --auth-mode login
-        echo '[+] File uploaded successfully!'
-        az storage blob list --container-name $1 --output table --auth-mode login
-        exit 0
-    fi
-fi
+#function to upload file to azure blob storage
+upload_to_azure() {
+    local container_name=containercli
+    local storage_account=azcliuploader
+    local bob_name=$(basename "$1")
 
+    #upload file to azure blob storage
+    echo "Uploading file to Azure Blob Storage..."
+    az storage blob upload --account-name "$storage_account" --container-name "$container_name" --file "$1" --name "$blob_name" --auth-mode login 2>&1 >/dev/null || { echo "File upload failed"; exit 1; }
+
+    # Display success message
+    echo "File successfully uploaded."
+
+    #prompt user to generate a shareable link
+    read -r  -p "Do you want to generate a shareable link for the file? [yes/no]: " generate_link
